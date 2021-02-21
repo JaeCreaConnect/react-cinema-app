599cf0fbe7f6ef6a96867f7c16efef9f633c49f5
Jae Regan <jae.creaconnect@gmail.com>
2021-02-21 12:34:59 -0500

    prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.scss';
 import { Provider } from 'react-redux';
 
-import Header from './components/header/Header'
+import Header from './components/header/Header';
 import store from './redux/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Header/>
+      <Header />
       <div className="app">
         <h1>Redux</h1>
       </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import './Header.scss'
-import logo from '../../assets/cinema-logo.svg'
+import './Header.scss';
+import logo from '../../assets/cinema-logo.svg';
 const HEADER_LIST = [
   {
     id: 1,
@@ -25,11 +25,8 @@ const HEADER_LIST = [
     iconClass: 'fas fa-plus-square',
     name: 'Upcoming',
     type: 'upcoming'
-  },
+  }
 ];
-
-
-
 
 const Header = () => {
   let [navClass, setNavClass] = useState(false);
@@ -38,60 +35,58 @@ const Header = () => {
   const toggleMenu = () => {
     menuClass = !menuClass;
     navClass = !navClass;
-    setNavClass(navClass)
+    setNavClass(navClass);
     setMenuClass(menuClass);
 
-    if (navClass){
+    if (navClass) {
       document.body.classList.add('header-nav-open');
-    }
-    else{
+    } else {
       document.body.classList.remove('header-nav-open');
     }
-  }
+  };
 
-  return(
+  return (
     <>
       <div className="header-nav-wrapper">
         <div className="header-bar"> </div>
-          <div className="header-navbar">
-            <div className="header-image">
-              <img src={logo} alt=""/>
-            </div>
-            <div
-              onClick={() => toggleMenu()}
-              className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`}
-              id="header-mobile-menu"
-            >
-
-              <span className="bar"></span>
-              <span className="bar"></span>
-              <span className="bar"></span>
-            </div>
-            <ul className={`${navClass ? 'header-nav header-mobile-nav' : 'header-nav'}`}>
-              {
-                HEADER_LIST.map((data) =>
-                  <li key={data.id}
-                      className="header-nav-item">
-                    <span className="header-list-name">
-                      <i className={data.iconClass}></i>
-                    </span>
-                    &nbsp;
-                    <span className="header-list-name">
-                      {data.name}
-                    </span>
-                  </li>
-                )
-              }
-              <input
-                className="search-input"
-                type="text"
-                placeholder="Search for a movie"
-              />
-            </ul>
+        <div className="header-navbar">
+          <div className="header-image">
+            <img src={logo} alt="" />
           </div>
+          <div
+            onClick={() => toggleMenu()}
+            className={`${
+              menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'
+            }`}
+            id="header-mobile-menu"
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <ul
+            className={`${
+              navClass ? 'header-nav header-mobile-nav' : 'header-nav'
+            }`}
+          >
+            {HEADER_LIST.map((data) => (
+              <li key={data.id} className="header-nav-item">
+                <span className="header-list-name">
+                  <i className={data.iconClass}></i>
+                </span>
+                &nbsp;
+                <span className="header-list-name">{data.name}</span>
+              </li>
+            ))}
+            <input
+              className="search-input"
+              type="text"
+              placeholder="Search for a movie"
+            />
+          </ul>
         </div>
+      </div>
     </>
-
-    );
+  );
 };
 export default Header;
